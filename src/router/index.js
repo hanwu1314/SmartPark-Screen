0c73343462bddd20e12fc07a88b54a94f3595b4a
hanwu <hanwu1314@outlook.com>
2023-09-25 20:38:48 +0800

    准备基础路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,21 @@
-import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
-
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import BigScreen from '../views/BigScreen/index.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: "/",
-      name: "home",
+      path: '/',
+      name: 'home',
       component: HomeView,
+      redirect: '/big-screen'
     },
-  ],
-});
+    {
+      path: '/big-screen',
+      name: 'bigScreen',
+      component: BigScreen
+    }
+  ]
+})
 
-export default router;
+export default router
---
+++ src/views/BigScreen/index.vue
@@ -0,0 +1,6 @@
+<script setup></script>
+<template>
+  <div>
+    <h2>大屏路由</h2>
+  </div>
+</template>
