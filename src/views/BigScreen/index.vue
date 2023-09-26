714b74f8b4bfa14633c2d2a7d7c53de1b40b12a3
hanwu <hanwu1314@outlook.com>
2023-09-26 10:58:32 +0800

    园区年度收入柱状图绘制

--- src/views/BigScreen/index.vue
+++ src/views/BigScreen/index.vue
@@ -1,5 +1,6 @@
 <script setup>
-import { ref } from 'vue'
+import * as echarts from 'echarts'
+import { onMounted, ref } from 'vue'
 import { getParkInfoAPI } from '@/services'
 
 const parkInfo = ref({})
@@ -9,17 +10,83 @@ async function getUserInfo() {
   parkInfo.value = res.data
 }
 
-getUserInfo()
+// 获取要渲染的节点位置
+const barChart = ref(null)
+/**初始化图表示例(必须保证dom是可用状态)*/
+function initChar(barChart) {
+  const { parkIncome } = parkInfo.value
+  const myChart = echarts.init(barChart.value)
+  const barOptions = {
+    tooltip: {
+      trigger: 'axis',
+      axisPointer: {
+        type: 'shadow'
+      }
+    },
+    grid: {
+      // 让图表占满容器
+      top: '10px',
+      left: '0px',
+      right: '0px',
+      bottom: '0px',
+      containLabel: true
+    },
+    xAxis: [
+      {
+        type: 'category',
+        axisTick: {
+          alignWithLabel: true,
+          show: false
+        },
+        data: parkIncome?.xMonth
+      }
+    ],
+    yAxis: [
+      {
+        type: 'value',
+        splitLine: {
+          show: false
+        }
+      }
+    ],
+    series: [
+      {
+        name: '园区年度收入',
+        type: 'bar',
+        barWidth: '10px',
+        data: parkIncome?.yIncome.map((item, index) => {
+          const color =
+            index % 2 === 0
+              ? new echarts.graphic.LinearGradient(0, 0, 0, 1, [
+                  { offset: 0.23, color: '#74c0f8' },
+                  { offset: 1, color: 'rgba(116,192,248,0.00)' }
+                ])
+              : new echarts.graphic.LinearGradient(0, 0, 0, 1, [
+                  { offset: 0.23, color: '#ff7152' },
+                  { offset: 1, color: 'rgba(255,113,82,0.00)' }
+                ])
+          return { value: item, itemStyle: { color } }
+        })
+      }
+    ],
+    textStyle: {
+      color: '#B4C0CC'
+    }
+  }
+  barOptions && myChart.setOption(barOptions)
+}
+
+onMounted(async () => {
+  await getUserInfo()
+  initChar(barChart)
+})
 </script>
 
 <template>
   <div class="all-charts">
     <!-- 园区概况 -->
     <div class="section-one">
-      <img
-        class="img-header"
-        src="https://yjy-teach-oss.oss-cn-beijing.aliyuncs.com/smartPark/%E5%A4%A7%E5%B1%8F%E5%88%87%E5%9B%BE/%E5%9B%AD%E5%8C%BA%E6%A6%82%E5%86%B5%402x.png"
-        alt="" />
+      <img class="img-header" src="@/assets/park-overview.png" alt="" />
       <div class="icons-container">
         <div class="item">
           <div class="icons-item building-icon">
@@ -58,6 +125,19 @@ getUserInfo()
           <span class="unity">（个）</span>
         </div>
       </div>
+    </div>
+    <!-- 园区年度收入分析 -->
+    <div class="section-two">
+      <img class="img-header" src="@/assets/park-annual-revenue.png" alt="" />
+      <div class="bar-chart-titile">
+        <span>单位：元</span>
+        <div>
+          <span class="bar-icon blue-bar-icon"></span>
+          <span class="bar-icon red-bar-icon"></span>
+          收入情况
+        </div>
+      </div>
+      <div class="bar-chart" ref="barChart"></div>
     </div>
   </div>
 </template>
@@ -142,6 +222,16 @@ getUserInfo()
         margin-top: 8px;
       }
     }
+  }
+}
+
+.section-two {
+  flex-basis: 35%;
+  margin-top: 50px;
+
+  .bar-chart {
+    width: 100%;
+    height: calc(100% - 90px);
   }
 }
 </style>
