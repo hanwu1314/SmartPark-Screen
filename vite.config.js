92b516d80be186a6f48e350c66252c67c7f13238
hanwu <hanwu1314@outlook.com>
2023-09-26 13:16:59 +0800

    微前端改造

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,20 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import qiankun from 'vite-plugin-qiankun'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    qiankun('smartpark-screen', {
+      useDevMode: true
+    })
   ],
+  server: {
+    // 防止开发阶段的assets 静态资源加载问题
+    origin: '//localhost:5173'
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
