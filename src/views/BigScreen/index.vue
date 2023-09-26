09301addc6e5995107acd7af2048de03bf6165b2
hanwu <hanwu1314@outlook.com>
2023-09-26 12:42:12 +0800

    可视化大屏

--- src/views/BigScreen/index.vue
+++ src/views/BigScreen/index.vue
@@ -3,6 +3,7 @@ import { onMounted, ref } from 'vue'
 import { useParkInfo, useInitBarChart, useInitPieChart } from '@/hooks'
 import { Application } from '@splinetool/runtime'
 import Loading from '@/component/Loading.vue'
+import VScaleScreen from 'v-scale-screen'
 const { parkInfo, getUserInfo } = useParkInfo()
 const { initBarChart, barChart } = useInitBarChart(parkInfo)
 const { initPieChart, pieChart } = useInitPieChart(parkInfo)
@@ -26,81 +27,83 @@ onMounted(async () => {
   await getUserInfo()
   initBarChart()
   initPieChart()
-  init3dModel()
+  // init3dModel()
 })
 </script>
 
 <template>
-  <Loading :loading="isShowLoading" />
-  <div class="all-charts">
-    <!-- 园区概况 -->
-    <div class="section-one">
-      <img class="img-header" src="@/assets/park-overview.png" alt="" />
-      <div class="icons-container">
-        <div class="item">
-          <div class="icons-item building-icon">
-            <span class="number">
-              {{ parkInfo.base?.buildingTotal }}
-            </span>
+  <v-scale-screen width="1920" height="1080">
+    <Loading :loading="isShowLoading" />
+    <div class="all-charts">
+      <!-- 园区概况 -->
+      <div class="section-one">
+        <img class="img-header" src="@/assets/park-overview.png" alt="" />
+        <div class="icons-container">
+          <div class="item">
+            <div class="icons-item building-icon">
+              <span class="number">
+                {{ parkInfo.base?.buildingTotal }}
+              </span>
+            </div>
+            <span class="title">楼宇总数</span>
+            <span class="unity">（栋）</span>
           </div>
-          <span class="title">楼宇总数</span>
-          <span class="unity">（栋）</span>
-        </div>
-        <div class="item">
-          <div class="icons-item enterprise-icon">
-            <span class="number">
-              {{ parkInfo.base?.chargePoleTotal }}
-            </span>
+          <div class="item">
+            <div class="icons-item enterprise-icon">
+              <span class="number">
+                {{ parkInfo.base?.chargePoleTotal }}
+              </span>
+            </div>
+            <span class="title">入驻企业总数</span>
+            <span class="unity">（家）</span>
           </div>
-          <span class="title">入驻企业总数</span>
-          <span class="unity">（家）</span>
-        </div>
-        <div class="item">
-          <div class="icons-item car-icon">
-            <span class="number">
-              {{ parkInfo.base?.enterpriseTotal }}
-            </span>
+          <div class="item">
+            <div class="icons-item car-icon">
+              <span class="number">
+                {{ parkInfo.base?.enterpriseTotal }}
+              </span>
+            </div>
+            <span class="title">车位总数</span>
+            <span class="unity">（个）</span>
           </div>
-          <span class="title">车位总数</span>
-          <span class="unity">（个）</span>
-        </div>
-        <div class="item">
-          <div class="icons-item rod-icon">
-            <span class="number">
-              {{ parkInfo.base?.parkingTotal }}
-            </span>
+          <div class="item">
+            <div class="icons-item rod-icon">
+              <span class="number">
+                {{ parkInfo.base?.parkingTotal }}
+              </span>
+            </div>
+            <span class="title">一体杆总数</span>
+            <span class="unity">（个）</span>
           </div>
-          <span class="title">一体杆总数</span>
-          <span class="unity">（个）</span>
         </div>
       </div>
-    </div>
-    <!-- 园区年度收入分析 -->
-    <div class="section-two">
-      <img class="img-header" src="@/assets/park-annual-revenue.png" alt="" />
-      <div class="bar-chart-titile">
-        <span>单位：元</span>
-        <div>
-          <span class="bar-icon blue-bar-icon"></span>
-          <span class="bar-icon red-bar-icon"></span>
-          收入情况
+      <!-- 园区年度收入分析 -->
+      <div class="section-two">
+        <img class="img-header" src="@/assets/park-annual-revenue.png" alt="" />
+        <div class="bar-chart-titile">
+          <span>单位：元</span>
+          <div>
+            <span class="bar-icon blue-bar-icon"></span>
+            <span class="bar-icon red-bar-icon"></span>
+            收入情况
+          </div>
         </div>
+        <div class="bar-chart" ref="barChart"></div>
       </div>
-      <div class="bar-chart" ref="barChart"></div>
+      <!-- 园区产业分布 -->
+      <div class="section-three">
+        <img
+          class="img-header"
+          src="https://yjy-teach-oss.oss-cn-beijing.aliyuncs.com/smartPark/%E5%A4%A7%E5%B1%8F%E5%88%87%E5%9B%BE/%E5%9B%AD%E5%8C%BA%E4%BA%A7%E4%B8%9A%E5%88%86%E5%B8%83%402x.png"
+          alt="" />
+        <div class="pie-chart" ref="pieChart"></div>
+      </div>
     </div>
-    <!-- 园区产业分布 -->
-    <div class="section-three">
-      <img
-        class="img-header"
-        src="https://yjy-teach-oss.oss-cn-beijing.aliyuncs.com/smartPark/%E5%A4%A7%E5%B1%8F%E5%88%87%E5%9B%BE/%E5%9B%AD%E5%8C%BA%E4%BA%A7%E4%B8%9A%E5%88%86%E5%B8%83%402x.png"
-        alt="" />
-      <div class="pie-chart" ref="pieChart"></div>
+    <div class="model-container">
+      <!-- 准备3D渲染节点 -->
+      <canvas class="canvas-3d" ref="ref3d" />
     </div>
-  </div>
-  <div class="model-container">
-    <!-- 准备3D渲染节点 -->
-    <canvas class="canvas-3d" ref="ref3d" />
-  </div>
+  </v-scale-screen>
 </template>
 
 <style scoped lang="scss">
